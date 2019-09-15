9ddc801acee782c2966bd7de2ca9f798d59dc6bb
Renan Henrique Da Fonte Costa <henriquebasshvf@gmail.com>
2019-09-15 04:16:23 -0300

    animation modal

--- src/views/InsureOptions/index.js
+++ src/views/InsureOptions/index.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react'
 import { Animated, Dimensions } from 'react-native'
 import { actions } from '../../redux/actions'
 import { connect } from 'react-redux'
-import { Container, TextView } from './styles'
+import {
+  Container,
+  BlueLine,
+  IconContainer,
+  Icon,
+  IconShare,
+  Content,
+  Square,
+  ContentOne,
+  TextInfo,
+  ButtonContainer,
+  ButtonText,
+  Button
+} from './styles'
 import PropTypes from 'prop-types'
 const { width, height } = Dimensions.get('window')
 
@@ -22,10 +35,14 @@ class InsureOptions extends Component {
     this.toggleMenu()
   }
 
+  closeModal = () => {
+    this.props.toggleModalAction()
+  }
+
   toggleMenu = () => {
     if (this.props.showModal) {
       Animated.spring(this.state.top, {
-        toValue: 54
+        toValue: 100
       }).start()
     }
     if (this.props.showModal == false) {
@@ -36,7 +53,36 @@ class InsureOptions extends Component {
   }
 
   render() {
-    return <Container style={{ top: this.state.top }}></Container>
+    return (
+      <Container style={{ top: this.state.top }}>
+        <IconContainer onPress={this.closeModal}>
+          <Icon name={'ios-close'} size={44} />
+        </IconContainer>
+        <Content>
+          <TextInfo>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </TextInfo>
+        </Content>
+        <Button>
+          <ButtonContainer>
+            <IconShare
+              color={'#454851'}
+              name={'ios-share-alt'}
+              style={{ marginright: 8 }}
+            />
+            <ButtonText>indicar</ButtonText>
+          </ButtonContainer>
+        </Button>
+      </Container>
+    )
   }
 }
 
@@ -50,4 +96,7 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(InsureOptions)
+export default connect(
+  mapStateToProps,
+  { ...actions.Animation }
+)(InsureOptions)
--- src/views/InsureOptions/styles.js
+++ src/views/InsureOptions/styles.js
@@ -1,17 +1,74 @@
 import styled from 'styled-components/native'
 import { Animated, Dimensions } from 'react-native'
+import { colors } from '../../styles'
+import ICON from 'react-native-vector-icons/Ionicons'
 const { width, height } = Dimensions.get('window')
 
 export const Container = styled(Animated.View)`
   position: absolute;
-  background: red;
+  background: #fff;
   height: ${height}px;
   width: 100%;
+  border-radius: 20px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
   z-index: 5;
 `
-export const TextView = styled.Text`
-  font-family: avenir;
+export const ContentOne = styled.View`
+  flex: 1;
+  background: red;
+`
+export const BlueLine = styled.View`
+  background: ${colors.blue};
+  height: 40px;
+`
+export const IconContainer = styled.TouchableOpacity`
+  position: absolute;
+  border-radius: 22;
+  top: 10px;
+  left: 50%;
+  margin-left: -22px;
+  height: 44px;
+  width: 44px;
+  justify-content: center;
+  align-items: center;
+  background: ${colors.blue};
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
+`
+export const Icon = styled(ICON).attrs({
+  size: 44,
+  color: 'white'
+})``
+export const IconShare = styled(ICON).attrs({
+  size: 36,
+  color: 'white'
+})``
+export const Content = styled.View`
+  margin-top: 100px;
+  justify-content: space-around;
+`
+export const TextInfo = styled.Text`
+  font-size: 16px;
+  padding: 15px;
+  color: ${colors.dark};
+`
+export const ButtonContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+`
+export const Button = styled.View`
+  height: 180px;
+  justify-content: center;
+  align-items: center;
+`
+export const ButtonText = styled.Text`
   font-size: 18px;
-  color: #fff;
   font-weight: bold;
+  color: white;
+  color: ${colors.dark}
+  margin-left: 8;
+`
+export const Square = styled.View`
+  background: red;
+  width: 100px;
+  height: 44px;
 `
