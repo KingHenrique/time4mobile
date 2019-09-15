da4c64b61d3b51f6c1d00a22610d5402fc3e753e
Renan Henrique Da Fonte Costa <henriquebasshvf@gmail.com>
2019-09-14 22:37:46 -0300

    add config to tab bar style

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,11 +2,48 @@ import React from 'react'
 import * as views from '../views'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-const AppNavigator = createBottomTabNavigator({
-  Home: { screen: views.Home },
-  Profile: { screen: views.Profile }
-  // Insert views here
-})
+const AppNavigator = createBottomTabNavigator(
+  {
+    Profile: { screen: views.Profile },
+    Home: { screen: views.Home }
+    // Insert views here
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => {
+      return {
+        tabBarIcon: ({ tintColor }) => {
+          const { routeName } = navigation.state
+          let IconComponent = Icon
+          let iconName
+          if (routeName === 'Home') {
+            iconName = 'ios-albums'
+          } else {
+            iconName = 'ios-person'
+          }
+          // You can return any component that you like here!
+          return <IconComponent name={iconName} size={30} color={tintColor} />
+        }
+        // tabBarOnPress: tabBarOnPress(navigation)
+      }
+    },
+    resetOnBlur: true,
+    tabBarOptions: {
+      pressOpacity: 0.6,
+      activeTintColor: 'red',
+      inactiveTintColor: '#0D2763',
+      style: {
+        paddingTop: 10,
+        height: 60
+      },
+      labelStyle: {
+        fontFamily: 'avenir',
+        fontWeight: '600',
+        fontSize: 11
+      }
+    }
+  }
+)
 
 export default createAppContainer(AppNavigator)
